e8258a407b000b0a74cb1050336313520635c9bb
riyas <riyastmuhammed@gmail.com>
2021-05-31 08:27:42 +0530

    restart

--- dbPool.js
+++ dbPool.js
@@ -7,5 +7,12 @@ var pool = mysql.createPool({
     password: 'secret',
     database: 'my_db'
 });
+// var pool = mysql.createPool({
+//     connectionLimit: 10,
+//     host: 'example.org',
+//     user: 'bob',
+//     password: 'secret',
+//     database: 'my_db'
+// });
 
 module.exports = pool;
